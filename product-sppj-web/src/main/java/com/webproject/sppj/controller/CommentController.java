1d94ae47c943a1a62aa007eb03321ab8c2075ef7
atguigu2017ybuq <atguigu_2017_ybuq@126.com>
2017-10-17 16:14:47 +0800

    controller实现

--- product-sppj-web/src/main/java/com/webproject/sppj/controller/CommentController.java
+++ product-sppj-web/src/main/java/com/webproject/sppj/controller/CommentController.java
@@ -20,14 +20,21 @@ public class CommentController {
 	@Autowired
 	private CommentService commentService;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	/**
+	 * 分页查询评价
+	 * @param pageNum
+	 * @param pageSize
+	 * @return
+	 */
+	@RequestMapping(value = "${pageNum}/${pageSize}", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<ProductComment>> queryCommentList(
 			@PathVariable("pageNum") Integer pageNum,
-			@PathVariable("pageSize")Integer pageSize) {
+			@PathVariable("pageSize") Integer pageSize) {
 
 		try {
-			List<ProductComment> list = commentService.queryList(pageNum, pageSize, null);
+			List<ProductComment> list = commentService.queryList(pageNum,
+					pageSize, null);
 			return ResponseEntity.ok(list);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -35,4 +42,91 @@ public class CommentController {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
 				null);
 	}
+
+	/**
+	 * 根据标签查询评价
+	 * @param labelString
+	 * @return
+	 */
+	@RequestMapping(value = "${labelString}", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<List<ProductComment>> queryCommentListByLabel(
+			@PathVariable("labelString") String labelString) {
+
+		try {
+			List<ProductComment> list = commentService
+					.queryCommentListByLabel(labelString);
+			return ResponseEntity.ok(list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+				null);
+	}
+
+	/**
+	 * 增加评价
+	 * @param comment
+	 * @return
+	 */
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<Void> addComment(ProductComment comment) {
+
+		try {
+			boolean addComment = commentService.addComment(comment);
+			if (addComment) {
+				return ResponseEntity.status(HttpStatus.CREATED).build();
+			} else {
+				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+				null);
+	}
+
+	/**
+	 * 更新评价
+	 * @param productComment
+	 * @return
+	 */
+	@RequestMapping(value = "", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<Boolean> updateComment(ProductComment productComment) {
+
+		try {
+			commentService.updateComment(productComment);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+				null);
+	}
+	
+	/**
+	 * 删除评价
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="${id}", method=RequestMethod.DELETE)
+	@ResponseBody
+	public ResponseEntity<Boolean> deleteComment(@PathVariable("id")Integer id){
+		
+		try {
+			commentService.deleteComment(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+				null);
+	}
+
 }
